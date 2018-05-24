8cd3476edecee54dac31098479236fc1fb53050f
Georgi Mirchev <georgi.mirchev@estafet.com>
2018-05-24 10:47:26 +0300

    Add note view model + save note

--- app/src/main/java/com/nmp90/mysimplediary/add/AddNoteActivity.kt
+++ app/src/main/java/com/nmp90/mysimplediary/add/AddNoteActivity.kt
@@ -1,12 +1,16 @@
 package com.nmp90.mysimplediary.add
 
 import android.app.DatePickerDialog
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.nmp90.mysimplediary.R
 import com.nmp90.mysimplediary.databinding.ActivityAddNoteBinding
 import com.nmp90.mysimplediary.utils.extensions.toSimpleString
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_add_note.*
 import java.util.*
 
@@ -14,19 +18,36 @@ import java.util.*
 class AddNoteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddNoteBinding
-    private var date: Date? = null
+    private var date: Date = Date()
+    private val disposables = CompositeDisposable()
+
+    private lateinit var viewModel: AddNoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_note)
         setSupportActionBar(toolbar)
 
-        binding.content?.tvDate?.text = Date().toSimpleString()
+        viewModel = ViewModelProviders.of(this).get(AddNoteViewModel::class.java)
+
+        binding.content?.tvDate?.text = date.toSimpleString()
         binding.content?.tvDate?.setOnClickListener({ onDatePick() })
         binding.fabSave.setOnClickListener { view ->
-            print(binding.content?.etNoteText?.text.toString())
+            val text = binding.content?.etNoteText?.text.toString()
+            val disposable = viewModel.saveNote(null, text, date)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        finish()
+                    })
+            disposables.add(disposable)
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        disposables.clear();
     }
 
 
@@ -34,7 +55,7 @@ class AddNoteActivity : AppCompatActivity() {
         val dialogFragment = DatePickerDialogFragment()
         dialogFragment.setListener(DatePickerDialog.OnDateSetListener { datePicker, i, i1, i2 ->
             date = GregorianCalendar(i, i1, i2).time
-            binding.content?.tvDate?.text = date?.toSimpleString()
+            binding.content?.tvDate?.text = date.toSimpleString()
         })
 
         dialogFragment.show(supportFragmentManager, DatePickerDialogFragment.TAG)
---
+++ app/src/main/java/com/nmp90/mysimplediary/add/AddNoteViewModel.kt
@@ -0,0 +1,21 @@
+package com.nmp90.mysimplediary.add
+
+import android.arch.lifecycle.ViewModel
+import com.nmp90.mysimplediary.MySimpleDiaryApp
+import com.nmp90.mysimplediary.notes.DbNotesRepository
+import com.nmp90.mysimplediary.notes.NotesRepository
+import io.reactivex.Completable
+import java.util.*
+
+
+class AddNoteViewModel : ViewModel() {
+    val notesRepository: NotesRepository;
+
+    init {
+        notesRepository = DbNotesRepository(MySimpleDiaryApp.instance)
+    }
+
+    fun saveNote(id: Long?, text: String, date: Date): Completable {
+        return notesRepository.saveNote(id, text, date)
+    }
+}
