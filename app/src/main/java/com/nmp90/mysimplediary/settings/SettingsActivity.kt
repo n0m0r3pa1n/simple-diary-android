da5a79dfe761df62f12925a4652cb1d0de94afcf
Georgi Mirchev <gmirchev90@gmail.com>
2019-05-22 18:57:27 +0300

    Toolbar in Settings

--- app/src/main/java/com/nmp90/mysimplediary/settings/SettingsActivity.kt
+++ app/src/main/java/com/nmp90/mysimplediary/settings/SettingsActivity.kt
@@ -4,8 +4,12 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceActivity
 import android.preference.PreferenceManager
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import com.nmp90.mysimplediary.R
-
+import androidx.annotation.LayoutRes
+import androidx.annotation.Nullable
+import androidx.appcompat.widget.Toolbar
 
 
 class SettingsActivity : PreferenceActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
@@ -13,13 +17,31 @@ class SettingsActivity : PreferenceActivity(), SharedPreferences.OnSharedPrefere
     private val PREF_ENABLE_NOTIFICATIONS = "pref_daily_notification"
 
     private lateinit var dailyNotificationController: DailyNotificationController
+    private var delegate: AppCompatDelegate? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        getDelegate().installViewFactory();
+        getDelegate().onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        setSupportActionBar(findViewById(R.id.toolbar));
+        getDelegate().supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         addPreferencesFromResource(R.xml.preferences);
+
         dailyNotificationController = DailyNotificationController(this)
 
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
@@ -29,6 +51,42 @@ class SettingsActivity : PreferenceActivity(), SharedPreferences.OnSharedPrefere
             dailyNotificationController.disableNotifications();
             dailyNotificationController.enableNotifications(sharedPreferences)
         }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        getDelegate().onPostCreate(savedInstanceState)
+    }
+
+    override fun setContentView(@LayoutRes layoutResID: Int) {
+        getDelegate().setContentView(layoutResID)
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        getDelegate().onPostResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        getDelegate().onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        getDelegate().onDestroy()
+    }
+
+    private fun setSupportActionBar(@Nullable toolbar: Toolbar) {
+        getDelegate().setSupportActionBar(toolbar)
+    }
+
+    private fun getDelegate(): AppCompatDelegate {
+        if (delegate == null) {
+            delegate = AppCompatDelegate.create(this, null)
+        }
+
+        return delegate!!
     }
 
 }
